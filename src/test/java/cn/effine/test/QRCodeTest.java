325f3d1db546d8746249e13c86d901ec9af90f5b
effine <verphen@163.com>
2015-10-10 18:31:21 +0800

    update

--- src/test/java/cn/effine/test/QRCodeTest.java
+++ src/test/java/cn/effine/test/QRCodeTest.java
@@ -7,13 +7,14 @@
 
 package cn.effine.test;
 
-import cn.effine.utils.QRCodeUtils;
 
 public class QRCodeTest {
 
 	public static void main(String[] args) {
-		String url = "https://www.baidu.com/";
-		QRCodeUtils.generateQRCode(200, 200, url, "d:/");
-		QRCodeUtils.generateQRCode(300, 300, "张亚飞", "d:/");
+		
+		
+		StringBuilder sb = new StringBuilder();
+		System.out.println(sb.length());
+		
 	}
 }
